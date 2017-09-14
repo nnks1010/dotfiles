8afdc29c17596a7c1693e52cb3c7991ec03915ac
nnks <ntomoya1010@gmail.com>
2017-09-14 23:43:11 +0900

    fix: remove upgrade

--- bin/test.sh
+++ bin/test.sh
@@ -12,7 +12,6 @@ if uname | fgrep -i Darwin > /dev/null 2>&1; then
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     fi
     brew update
-    brew upgrade --all
     brew doctor
     set +x
 
@@ -24,7 +23,6 @@ else
     echo "update apt"
     set -x
     sudo apt-get update -y
-    sudo apt-get dist-upgrade -y
     set +x
 
     echo "install git"
