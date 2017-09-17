1a0bb7ad04426e7de65888a7cda0f8e3e109b5ad
nnks <ntomoya1010@gmail.com>
2017-09-17 23:20:34 +0900

    update: update package

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+echo "setup symbolic link"
 set -x
 
 cd etc/
@@ -14,3 +16,20 @@ done
 cd ../
 
 set +x
+
+echo "update package"
+
+if uname | fgrep -i Darwin > /dev/null 2>&1; then
+    brew update
+    brew upgrade
+    brew cleanup -n
+    brew doctor
+else
+    sudo apt update -y
+    sudo apt upgrade -y
+    sudo apt autoremove
+    sudo apt autoclean
+fi
+
+echo "update anyenv"
+anyenv update
