2ae800bac8f3070a3a413cc973f4bf32fb6362dc
nnks <ntomoya1010@gmail.com>
2017-09-17 21:23:43 +0900

    fix: ln directory

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+set -x
 
 cd etc/
-
 PWD=`pwd`
-
 for file in .??*
 do
+    if [ -d $file ] && [ -e $HOME/$file ]; then
+        echo $file
+        rm -rf $HOME/$file
+    fi
     ln -sf $PWD/$file $HOME/$file
 done
-
 cd ../
+
+set +x
